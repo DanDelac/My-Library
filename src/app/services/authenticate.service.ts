986818eb6b9d0eb4c8d2688d582e31b572399afe
PCA <71424261@continental.edu.pe>
2023-01-20 17:35:17 -0500

    clase 20/01/23 menu

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
 
   loginUser(credentials: any){
     return new Promise((accept, reject) =>{
-      if ( credentials.email == "andrea@gmail.com" && credentials.password == "123456")
+      if ( credentials.email == "andrea@gmail.com" && credentials.password == "123456" )
       {
-        
         accept("Login Exitoso");
       } else {
         reject("Login Fallido");
       }
     });
+  }
+
+  registerUser(userData: any){
+    userData.password = btoa(userData.password);
+    return this.storage.set("user", userData);
   }
 }
